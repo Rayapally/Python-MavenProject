1c9dbeaafc54bb7c9befa67cbed0bc1a2af61e7f
Rayapally <rayapallyvikas1@gmail.com>
2024-02-13 15:45:07 +0530

    Give me some sunshine

---
+++ my_module.py
@@ -0,0 +1,26 @@
+# my_module.py
+
+def add(x, y):
+    """Addition function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction function"""
+    return x - y
+
+
+# test_my_module.py
+
+#import pytest
+from my_module import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 1) == 0
+    assert subtract(-1, 1) == -2
+
---
+++ test_my_module.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_subtract(self):
+        self.fail()
